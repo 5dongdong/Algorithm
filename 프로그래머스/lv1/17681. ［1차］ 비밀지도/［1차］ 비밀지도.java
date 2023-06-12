93878cc98c4c10c8a709c2a6d8e083c177488886
Odongdong <dhgus44@naver.com>
2023-06-12 17:59:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,18 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[arr1.length];
-
-        for (int i = 0; i < arr1.length; i++) {
-            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-            while(answer[i].length() < arr1.length){
-                answer[i] = "0" + answer[i];
-            }
-            answer[i] = answer[i].replace("1", "#").replace("0", " ");
-        }
-
+        String[] answer = {};
         return answer;
     }
 }
-
