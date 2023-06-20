5f8834b6f2a66e307b9ec6ce00a9d380cca0b04e
Odongdong <dhgus44@naver.com>
2023-06-20 19:30:10 +0900

    [unrated] Title: 코드 처리하기, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String code) {
+        if (code.isEmpty()) {
+            return "EMPTY";
+        }
+        StringBuilder ret = new StringBuilder();
+        boolean mode = false; 
+
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            if (mode == false) {
+                if (i % 2 == 0 && c != '1') {
+                    ret.append(c);
+                } else if (c == '1') {
+                    mode = true;
+                }
+            } else {
+                if (i % 2 == 1 && c != '1') {
+                    ret.append(c);
+                } else if (c == '1') {
+                    mode = false;
+                }
+            }
+        }
+        
+        if (ret.length() == 0) {
+            return "EMPTY";
+        } else {
+            return ret.toString();
+        }
+    }
+}
