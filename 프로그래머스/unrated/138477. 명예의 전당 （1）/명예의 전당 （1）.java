638fdf3ef77d7152b575685891ff34f4fdeed3dd
Odongdong <dhgus44@naver.com>
2023-07-18 17:41:05 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.46 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,17 +1,6 @@
-import java.util.*;
-
 class Solution {
-    public static int[] solution(int k, int[] score) {
-        int[] answer = new int[score.length];
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-
-        for (int i = 0; i < score.length; i++) {
-            q.add(score[i]);
-            if (q.size() > k) {
-                q.poll();
-            }
-            answer[i] = q.peek();
-        }
+    public int[] solution(int k, int[] score) {
+        int[] answer = {};
         return answer;
     }
 }
